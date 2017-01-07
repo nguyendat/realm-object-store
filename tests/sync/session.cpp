7f87a5eed2aa846e9009656b873fd9acfdbd5774
Austin Zheng <austinzheng@gmail.com>
2017-01-06 16:18:41 -0800

    Added a few basic tests

--- tests/sync/session.cpp
+++ tests/sync/session.cpp
@@ -271,3 +271,51 @@ TEST_CASE("sync: log-in", "[sync]") {
 
     // TODO: write a test that logs out a Realm with multiple sessions, then logs it back in?
 }
+
+TEST_CASE("sync: progress notification", "[sync]") {
+    if (!EventLoop::has_implementation())
+        return;
+
+    auto cleanup = util::make_scope_exit([=]() noexcept { SyncManager::shared().reset_for_testing(); });
+    SyncServer server;
+    // Disable file-related functionality and metadata functionality for testing purposes.
+    SyncManager::shared().configure_file_system("/tmp/", SyncManager::MetadataMode::NoMetadata);
+    auto user = SyncManager::shared().get_user("user", "not_a_real_token");
+
+    SECTION("runs at least once (initially when registered)") {
+        auto user = SyncManager::shared().get_user("user1a", "not_a_real_token");
+        auto session = sync_session(server, user, "/test1a",
+                                     [](auto&, auto&) { return s_test_token; },
+                                     [](auto, auto) { },
+                                     SyncSessionStopPolicy::AfterChangesUploaded);
+        EventLoop::main().run_until([&] { return session_is_active(*session); });
+        REQUIRE(!session->is_in_error_state());
+        std::atomic<bool> callback_was_called(false);
+
+        SECTION("for upload notifications, with no data transfer ongoing") {
+            uint64_t token = session->register_progress_notifier([&](auto, auto) {
+                callback_was_called = true;
+            }, SyncSession::NotifierType::upload, false);
+            REQUIRE(token == 0);
+            EventLoop::main().run_until([&] { return callback_was_called.load(); });
+        }
+
+        SECTION("for download notifications, with no data transfer ongoing") {
+            uint64_t token = session->register_progress_notifier([&](auto, auto) {
+                callback_was_called = true;
+            }, SyncSession::NotifierType::download, false);
+            REQUIRE(token == 0);
+            EventLoop::main().run_until([&] { return callback_was_called.load(); });
+        }
+
+        SECTION("can register another notifier while in the initial notification without deadlock") {
+            session->register_progress_notifier([&](auto, auto) {
+                callback_was_called = true;
+                session->register_progress_notifier([&](auto, auto) {
+                    callback_was_called = true;
+                }, SyncSession::NotifierType::upload, false);
+            }, SyncSession::NotifierType::download, false);
+            EventLoop::main().run_until([&] { return callback_was_called.load(); });
+        }
+    }
+}
